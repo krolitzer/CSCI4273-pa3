d1a75e816f4e93bd8a478f9227f7c5bc68085c92
chris-costello6 <christopher.costello6@gmail.com>
2014-10-26 14:47:04 -0600

    set up data structures

--- EventScheduler.h
+++ EventScheduler.h
@@ -8,17 +8,45 @@
 #include <stdio.h>
 #include <iostream>
 #include <map>
+#include <queue>
+#include <ctime>
+#include <chrono>
+#include <vector>
 
 using namespace std;
+/** 
+ * Making this global to be accessable by all classes
+ * Still looking for a better solution.
+ **/
+struct EventTime
+{
+	int eventID;
+	chrono::milliseconds execTime;
+};
+	
+/** Making new class for priority queue compatability */
+class CompareEventTime {
+public:
+	/**
+	 * Used by priority queue to order based on exec time.
+	 * Orders earliest first.
+	 **/
+	bool operator()(EventTime &e1, EventTime &e2) {
+		if (e2.execTime < e1.execTime) {
+			return true;
+		}
+		return false;
+	}
+};
 
 class EventScheduler {
-
 public:
 	EventScheduler(size_t maxEvents);
 	~EventScheduler();
 	int eventSchedule(void evFunction(void *), void* arg, int timeout);
 	void eventCancel(int eventID);
-
+	
+	
 private:
 	int num_events;
 	int max_events;
@@ -26,36 +54,45 @@ private:
 	// schedule execution with priority queue where
 	// the smallest delay has highest priority
 
-	struct functionInfo
+	struct FunctionInfo
 	{
 		void evFunction(void *);
 		void* arg;
 	};
-
-	map<int, functionInfo> eventFunctionMap;
 	
-}
+	map<int, FunctionInfo> eventFunctionMap;
+	priority_queue<EventTime, vector<EventTime>, CompareEventTime> functionQueue;
+};
+
+
+
 
 EventScheduler::EventScheduler(size_t maxEvents) {
 	max_events = maxEvents;
 
-	cout << "Scheduler created with max " + max_events " events." << endl;
+	cout << "Scheduler created with max " << max_events << " events." << endl;
 }
 
 EventScheduler::~EventScheduler() {
-	// delete data structures
+	delete eventFunctionMap;
+	delete functionQueue;
 }
 
 int 
 EventScheduler::eventSchedule(void evFunction(void *), void* arg, int timeout) {
-	cout << "Scheduling the function with timeout" << endl;
+	// Decide if timeout is seconds or milliseconds or something else
+	// Internally will handle time as milli seconds.
+
+
+	cout << "Scheduling the function with timeout " << timeout << endl;
+
 	return 0;
 }
 
 void
-eventCancel(int eventID) {
-	cout << "Cancelled event " + event << endl;
+EventScheduler::eventCancel(int eventID) {
+	// remove entry from map and priority queue.
+	cout << "Cancelled event " << eventID << endl;
 }
-
 
 
