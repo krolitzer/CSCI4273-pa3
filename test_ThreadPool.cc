9f059a233b961e297a573096fab19276248e7927
Forerunner117 <alongo117@gmail.com>
2014-11-06 11:15:46 -0700

    Implemented pthread_create function call

--- test_ThreadPool.cc
+++ test_ThreadPool.cc
@@ -4,7 +4,9 @@ int main(int argc, char* argv[])
 {
 	ThreadPool pool1 = ThreadPool();
 	ThreadPool pool2 = ThreadPool(50);
+	ThreadPool th(3);
 
 	cout << "Available Threads? " << pool1.thread_avail() << endl;
 	cout << "Available Threads? " << pool2.thread_avail() << endl;
+	cout << "Available Threads? " << th.thread_avail() << endl;
 }
